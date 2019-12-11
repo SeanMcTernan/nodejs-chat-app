44cb368c6707a0a186e3fb4921591a116379ca72
Sean Mc Ternan <sean@beyondthesummitpdd.com>
2019-12-10 19:10:33 -0700

    Render location messges into the screen and style the app

--- src/index.js
+++ src/index.js
@@ -2,6 +2,8 @@ const express = require('express')
 const http = require('http')
 const path = require('path')
 const socketio = require('socket.io')
+const Filter = require('bad-words')
+const { generateMessage, generateLocationMessage } = require('./utils/messages')
 
 const app = express()
 const server = http.createServer(app)
@@ -12,10 +14,32 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-io.on('connection', () => {
-    console.log('New websocket connection')
-})  
+io.on('connection', (socket) => {
+    
+    socket.emit('message', generateMessage('Welcome!'))
 
+    socket.broadcast.emit('message', generateMessage('A new user has joined!'))
+
+    socket.on('sendMessage', (message, callback) => {
+        const filter = new Filter()
+
+        if (filter.isProfane(message)) {
+            return callback('Profanity is not allowed!')
+        }
+
+        io.emit('message', generateMessage(message))
+        callback()
+    })
+
+    socket.on('disconnect', () => {
+        io.emit('message', generateMessage('A user has left!)'))
+    })
+
+    socket.on('sendLocation', (location, callback) => {
+        io.emit('locationMessage', generateLocationMessage(`https://google.com/maps?q=${location.latitude},${location.longitude}`))
+        callback()
+    })
+})  
 
 server.listen(port, () => {
     console.log('Server is up on port ' + port)
